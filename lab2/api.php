a2abaebb665f407e58779cabef3af14c0d7bed72
Rezakien <amirkhanzukhritdinov@gmail.com>
2019-04-22 12:17:20 +0500

    Updates in file status.txt 22/04/2019

---
+++ lab2/api.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * API CLASS
+ */
+class API
+{
+	protected $db = null;
+	function __construct($db)
+	{
+		$this->$db = $db;
+	}
+	function create($name){
+
+	}
+	function change(){
+		
+	}
+	function delete(){
+		
+	}
+	function create(){
+		
+	}
+}
+?>
---
+++ lab2/index.php
@@ -0,0 +1,13 @@
+<?php
+header("Request-Type: POST");
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>lab2</title>
+</head>
+<body>
+
+</body>
+</html>
